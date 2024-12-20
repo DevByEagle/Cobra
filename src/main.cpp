0c5233af0ca070bd7127aab0a140714d0928a953
Aaron Thompson <athompsonbriley.56@gmail.com>
2024-12-19 21:16:59 -0500

    DEV: Updated Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "parser.h"
+#include "lexer.h"
 
-int main() {
-  std::cout << "Hello World" << std::endl;
+std::string readFile(const std::string &filename) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        exit(1);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cout << "USAGE: %s <file_to_compile>" << argv[0] << std::endl;
+    std::exit(0);
+  }
+
+  std::string code = readFile(argv[1]);
+  pygo::Lexer lexer(code);
+  pygo::Parser parser(lexer);
+
+  parser.parse();
+
   return 0;
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -13,4 +13,54 @@ namespace pygo {
       currentToken == lexer.nextToken();
     }
   }
+  // Private Functions
+  void Parser::constStatement() {
+    currentToken == lexer.nextToken();; // consume "const"
+    if (currentToken.type != Token::IDENTIFIER) {
+      error("Expected identifier after 'const'");
+    }
+    std::string varName = currentToken.value;
+
+    currentToken = lexer.nextToken(); // consume identifier
+    if (currentToken.type != Token::ASSIGNMENT) {
+      error("Expected '=' after identifier");
+    }
+
+    currentToken = lexer.nextToken(); // consume '='
+    if (currentToken.type != Token::INTEGER) {
+      error("Expected integer after '='");
+    }
+    int value = std::stoi(currentToken.value);
+
+    currentToken = lexer.nextToken(); // consume integer
+    if (currentToken.type != Token::SEMICOLON) {
+      error("Expected ';' after expression");
+    }
+
+    variables[varName] = value;
+    std::cout << "Variable " << varName << " assigned value " << value << std::endl;
+  }
+
+  void Parser::printStatement() {
+    currentToken = lexer.nextToken(); // consume "print"
+    if (currentToken.type != Token::IDENTIFIER) {
+      error("Expected identifier after 'print'");
+    }
+    std::string varName = currentToken.value;
+
+    currentToken = lexer.nextToken(); // consume identifier
+    if (currentToken.type != Token::SEMICOLON) {
+      error("Expected ';' after expression");
+    }
+
+    if (variables.find(varName) == variables.end()) {
+      error("Variable not found");
+    }
+    std::cout << "Print: " << variables[varName] << std::endl;
+  }
+
+   void error(const std::string &message) {
+     std::cerr << "Error: " << message << std::endl;
+     exit(1);
+   }
 }
